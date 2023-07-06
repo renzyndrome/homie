0a23643d8beb5d77463bba8d77fdf669db0299db
Laurence Rebadulla <gabrielrebadulla@gmail.com>
2023-07-06 11:11:15 +0800

    feat: add custom user model

--- homie_core/urls.py
+++ homie_core/urls.py
@@ -6,3 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('myadmin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "Homie Admin"
+admin.site.site_header = "Homie Admin Portal"
+admin.site.site_header = "Welcome to Homie"
